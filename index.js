c7d02005d72ff2ab725f428b906a9118e89b7c09
kdreps <szatikoko@gmail.com>
2026-04-23 19:21:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,11 +95,6 @@ client.on("messageCreate", async (message) => {
       try {
         const res = await axios.get(searchUrl, {
   headers: {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-  }
-});
-        const res = await axios.get(searchUrl, {
-  headers: {
     "User-Agent": "Mozilla/5.0"
   }
 });
